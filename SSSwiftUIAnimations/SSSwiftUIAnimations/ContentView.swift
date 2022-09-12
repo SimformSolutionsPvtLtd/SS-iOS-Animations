3c9f8286583e7e493b30764b183bffca182a25a1
Purva Ruparelia <purva.r@simformsolutions.com>
2022-09-12 15:29:37 +0530

    Xcode project created

---
+++ SSSwiftUIAnimations/SSSwiftUIAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SSSwiftUIAnimations
+//
+//  Created by Purva Ruparelia on 12/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
